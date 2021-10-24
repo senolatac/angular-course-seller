f1b12cd2a74c4ba59f96ae62e428e1dad2191ac7
Senol Atac <senol.atac.1@gmail.com>
2021-10-24 17:18:16 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { CourseSaveComponent } from './components/course-save/course-save.component';
+import { CourseDeleteComponent } from './components/course-delete/course-delete.component';
+import {authInterceptorProviders} from "./interceptors/auth.interceptor";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    AdminComponent,
+    NotFoundComponent,
+    UnauthorizedComponent,
+    CourseSaveComponent,
+    CourseDeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { faUserGraduate } from '@fortawesome/free-solid-svg-icons';
+import {Course} from "../../models/course.model";
+import {AuthenticationService} from "../../services/authentication.service";
+import {CourseService} from "../../services/course.service";
+import {PurchaseService} from "../../services/purchase.service";
+import {Purchase} from "../../models/purchase.model";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  courseList: Array<Course> = [];
+  faUserGraduate = faUserGraduate;
+  errorMessage: string = "";
+  infoMessage: string = "";
+
+  constructor(private authenticationService: AuthenticationService,
+              private courseService: CourseService,
+              private purchaseService: PurchaseService) { }
+
+  ngOnInit(): void {
+    this.courseService.getAllCourses().subscribe(data => {
+      this.courseList = data;
+    });
+  }
+
+  purchase(item: Course) {
+    if (!this.authenticationService.currentUserValue?.id) {
+      this.errorMessage = 'You should login to buy a course';
+      return;
+    }
+
+    const purchase = new Purchase(this.authenticationService.currentUserValue.id, item.id, item.title, item.price);
+
+    this.purchaseService.savePurchase(purchase).subscribe(data => {
+      this.infoMessage = 'Mission is completed';
+    }, err => {
+      this.errorMessage = 'Unexpected error occurred.';
+      console.log(err);
+    });
+  }
+
+}
